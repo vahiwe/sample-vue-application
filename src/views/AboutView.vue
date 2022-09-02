a431005695dd57467d5fa5144b50e29dcb015206
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2022-09-02 07:42:47 +0100

    Initial Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const name = import.meta.env.VITE_AGENT_NAME;
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page - {{ name }} </h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
